1ae8675a0aa23e30b55a706294d75d8be78f699c
Emilien Escalle <neilime@users.noreply.github.com>
2020-09-10 16:53:08 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,36 @@
-
 #!/bin/bash
 
+ppas=( ppa:utappia/stable )
+softwares=( ucaresystem-core )
+
 ###################
-# Install ucaresystem-core #
-if ! grep -q "ansible/ansible" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
-    echo "Adding Utappia PPA"
-    sudo apt-add-repository ppa:utappia/stable -y
+# Install PPAs #
+for i in "${ppas[@]}"
+do 
+  if ! grep -q "^deb .*$the_ppa" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+    echo "Adding $i PPA"
+    sudo apt-add-repository $i -y
+  fi
+done
+
+# Check for non installed softwares
+softwaresToInstall=""
+for i in "${softwares[@]}"
+do 
+   if ! hash ucaresystem-core >/dev/null 2>&1; then
+     softwaresToInstall="${softwaresToInstall} $i"
+   fi
+done
+
+if [ ! -z "$softwaresToInstall" ]
+then
+ echo "Installing $softwaresToInstall..."
+ 
+ sudo apt update
+ sudo apt install $softwaresToInstall
+ 
+ echo "Installation done"
 fi
 
-sudo apt update
-sudo apt install ucaresystem-core
+echo "Cleaning..."
+sudo ucaresystem-core
