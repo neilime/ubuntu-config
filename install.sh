b28fb2a2ccb8eb8df22583d0eba2f4b2e8f4f50c
Emilien Escalle <neilime@users.noreply.github.com>
2020-09-11 15:03:20 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,58 +1,82 @@
 #!/bin/bash
 
-ppas=( ppa:utappia/stable )
-aptSoftwares=( ucaresystem-core )
+ppas=( utappia/stable )
+aptSoftwares=( ucaresystem-core git zsh copyq bat yarn chromium-browser )
 snapSoftwares=( code spotify )
 
-# Install zsh
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+echo "Start installation..."
 
 # Install PPAs #
 for i in "${ppas[@]}"
 do 
   if ! grep -q "^deb .*$i" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
     echo "Adding $i PPA"
-    sudo apt-add-repository "$i" -y
+    sudo apt-add-repository "ppa:$i" -y
   fi
 done
+
+# Configure Yarn Debian package repository
+if [ ! -f /etc/apt/sources.list.d/yarn.list ]; then 
+  wget -qO- https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+  echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+fi
 
 # Check for non installed apt softwares
 aptSoftwaresToInstall=""
 for i in "${aptSoftwares[@]}"
 do 
-   if ! hash "$i" >/dev/null 2>&1; then
+  if [ $(dpkg-query -W -f='${Status}' $i 2>/dev/null | grep -c "ok installed") -eq 0 ];
+  then
      aptSoftwaresToInstall="${aptSoftwaresToInstall} $i"
-   fi
+  fi
 done
 
 if [ -n "$aptSoftwaresToInstall" ]
 then
- echo "Installing $aptSoftwaresToInstall..."
+ echo "Installing apt $aptSoftwaresToInstall..."
  
  sudo apt update
- sudo apt install -y "$aptSoftwaresToInstall"
+ sudo apt install -y $aptSoftwaresToInstall
  
- echo "Installation done"
+ echo "Apt installation done"
 fi
 
-# Check for non installed snap softwares
-snapSoftwaresToInstall=""
+# Install snap softwares
 for i in "${snapSoftwares[@]}"
 do 
    if ! hash "$i" >/dev/null 2>&1; then
-     snapSoftwaresToInstall="${snapSoftwaresToInstall} $i"
+     echo "Installing snap $i..."
+     sudo snap install --classic "$i"
+     echo "Snap installation done"
    fi
 done
 
-if [ -n "$snapSoftwaresToInstall" ]
-then
- echo "Installing $snapSoftwaresToInstall..."
- 
- sudo snap install --classic "$snapSoftwaresToInstall"
- 
- echo "Installation done"
+# Install nvm
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+# Install oh-my-zsh
+if [ ! -f ~/.zshrc ] && [ ! -h ~/.zshrc ]; then 
+  sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+else
+  zsh -i -c "omz update"
 fi
 
+# Setup nerdfont
+# wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.zip -P /tmp
+# unzip -o /tmp/FiraCode.zip -d ~/.fonts
+# rm -f /tmp/FiraCode.zip
+# fc-cache -fv
+
+# Setup antigen & starship
+wget git.io/antigen -O ~/antigen.zsh
+wget https://raw.github.com/neilime/ubuntu-config/master/antigenrc -O ~/.antigenrc
+zsh -i -c "$(wget https://starship.rs/install.sh -O -)" '' -f
+
+wget https://raw.github.com/neilime/ubuntu-config/master/zshrc -O ~/.zshrc
 
 echo "Cleaning..."
-# sudo ucaresystem-core
+#sudo ucaresystem-core
+
+echo "Installation done"
+
+zsh -i -c "exec zsh"
